6201d22219adfe21e6f7be85d109e598ba9c2daa
IMRegutierrez <egutierrez@imr.com.mx>
2025-07-31 10:10:31 -0600

    logo size 64

--- src/app/PortalClientComponent.tsx
+++ src/app/PortalClientComponent.tsx
@@ -162,7 +162,7 @@ export default function PortalClientComponent({ config }: { config: ClientConfig
                         <img
                             src={config.logoUrl}
                             alt={`Logo de ${config.clientName}`}
-                            className="h-32 w-auto mx-auto mb-4 object-contain"
+                            className="h-64 w-auto mx-auto mb-4 object-contain"
                         />
                     ) : (
                         <svg className="w-16 h-16 mx-auto mb-4 text-slate-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
